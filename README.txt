0d6a4037c58ca96aed53de7a126820b47e2d8ebb
jmatta697 <jmatta1980@hotmail.com>
2018-11-20 23:32:28 -0500

    final update

--- README.txt
+++ README.txt
@@ -1,45 +1,60 @@
-bc is a command line calculator, type bc at the command line of most linux systems and you get a copyright notice then
-the program waits for you to type in a bit of math, then bc will evaluate that math and print an answer, then wait for
-the user to type more.
+Author: Joe Matta
+Copyright 2018 - Joe Matta. All rights reserved.
 
+This program aims to imitate bc which is a command line calculator. Typing bc at the command line of most linux systems
+will bring up a copyright notice then the program waits for you to type in a bit of math, it will then evaluate that math
+and print an answer, then wait for the user to type more.
 
-Your subset of bc needs to
+This implementation of bc does the following:
 
-
-support arithmetic (+ - * /)
-support parenthesis
-support variables
-variables that are referenced but not yet assigned automatically get a value of zero.
-allow assignment to variables using =
-Support comparisons < =< etc. (return 1 for true and 0 for false)
-support passing arithmetic expressions on the command line as command line parameters when starting bc
+supports arithmetic (+ - * /)
+supports parenthesis including nested parentheses
+supports variables -
+    variables that are referenced but not yet assigned automatically get a value of zero.
+    allows assignment to variables using =
+supports comparisons < =< etc. (return 1 for true and 0 for false)
+supports passing arithmetic expressions on the command line as command line parameters when starting matta_bc
 eg:
-bc 5+3
-should invoke bc for the single computation, print the answer and then exit the program.
-support using the help flag: bc --help should explain what your subset of bc can do.
+entering the following in the command line: matta_bc 5+3
+    will invoke matta_bc for the single computation, print the answer of 8 and then exit the program.
+help (this document) is available through the help flag: matta_bc --help
+    - this will bring up this document explaining how to use matta_bc
 
-source for expression evaluation algorithm:
-https://www.geeksforgeeks.org/expression-evaluation/
+To run the program from the command line:
+Navigate to the folder that holds the executable matta_bc (file with no extension) and type the following:
+$ ./matta_bc
+Calling the program with no command line arguments will allow the user to enter an expression and obtain a result as
+many times as they would like until 'quit' is entered. Entering 'quit' ends the program.
+The user may pass a command line argument:
+$ ./matta_bc 5+3
+If more that one argument is passed to the program call, an error will occur.
+$ ./matta_bc 5+3 7-2  ---> error
+If the user includes spaces, parentheses, '<', or '>', single quotes are required around the argument. Some other cases
+may exist that necessitate the use of quotes. Thus, the user is advised to always use quotes when passing a command line
+argument.
+$ ./matta_bc '(3+3)/2'
 
 notes:
--multiple digit numbers cannot have spaces between digits (ex. 32 cannot be expressed as 3 2)
--multiplication cannot be done by (2)(3) or 2(3), must be (2)*(3) or 2*(3).
--variable names must start with a lowercase letter. They may only contain lowercase letters and numbers.
--when passing a command line argument without quotes around it, argument cannot contain spaces or parentheses
--variables can NOT be assigned to other variables (ie. 'variable0 = variable1' is not valid)
--when evaluating comparisons, only one set of comparison notation is allowed (ie. '3+4<=5==0' is not allowed)
--if assigning a value to a variable, user input must start with a lowercase letter followed by any number of any
-    combination of lowercase letters and numbers followed by '='. The assigned value may be a mathematical expression
-    or a comparison of expressions.
--can NOT assign a value of '0' to a variable by simply typing a variable name and pressing enter (ie. entering
-    'variable2' and then pressing enter will not store the variable and assign a zero value, it will result in an
-    error.
--dividing by 0 explicitly (ie. entering '3/0') will cause the an error and the user will be prompted to enter a valid
+- Multiple digit numbers cannot have spaces between digits (ex. 32 cannot be expressed as 3 2) when passing an expression
+    as a command line argument without quotes
+- Multiplication cannot be done by (2)(3) or 2(3), must be (2)*(3) or 2*(3).
+- Variable names must start with a lowercase letter. They may only contain lowercase letters and numbers.
+- Variable names must end in numbers, if they include numbers. Varibale names cannot have numbers inside name
+    (ie. 'var3iable' is not a valid variable name, but 'variable33' is)
+- When passing a command line argument without quotes around it, argument cannot contain spaces or parentheses
+- Variables can NOT be assigned to other variables (ie. 'variable0 = variable1' is not valid)
+- If assigning a value to a variable, user input must start with a lowercase letter followed by any number of any
+    combination of lowercase letters and then followed by numbers and then '='. The assigned value may be a
+    mathematical expression or a comparison of expressions.
+- Dividing by 0 explicitly (ie. entering '3/0') will cause the an error and the user will be prompted to enter a valid
     expression. Dividing by 0 implicitly (ie. entering '3/(2-2)' will cause a fatal error; a message will be printed
     and the program will exit.
--When entering an expression as a command line argument, the user is required to surround the expression with
+- When entering an expression as a command line argument, the user is required to surround the expression with
     single quotes in some cases, specifically in the case of comparisons using '<' or '>'. Single quotes must also be
     used when the expression contains parentheses. For most other cases, the expression can be entered without being
     surrounded by quotes. This is the result of this notation coinciding with bash commands.
     Example: 34<=100 must be entered as '34<=100' as a command line argument
     Example: (3+3)/2 must be entered as '(3+3)/2' as a command line argument
+
+source for expression evaluation algorithm:
+https://www.geeksforgeeks.org/expression-evaluation/
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::io;
 use std::process;
 use std::collections::HashMap;
+use regex::Regex;
 
 
 fn main() {
@@ -14,7 +15,11 @@ fn main() {
         println!("\nToo many command line arguments! Enter one argument or none. Exiting program...\n");
     }
     else if command_line_arg.trim() == "" {
+        println!("{}", "Copyright 2018 - Joe Matta. All rights reserved.");
         main_user_input_loop(variable_map);
+    }
+    else if command_line_arg.trim() == "--help" {
+        print_help_text();
     }
     else {
         main_one_time_eval(command_line_arg, variable_map);
@@ -25,12 +30,16 @@ fn main() {
 
 /// this function processes the one-time evaluation when program is called with a command line arg
 fn main_one_time_eval(command_line_arg: String, varib_map: &mut HashMap<String, String>) {
+
+    let regex_pat = Regex::new(r"^[a-z 0-9+\-*/()=<>]*$").unwrap();
+
     // clean all white space from string and any trailing or heading escape chars
     let clean_white_space_str = remove_all_white_space_from_string(command_line_arg.trim());
 
-    // ***CHECK FOR VALID INPUT HERE!!****
-        // if not valid - print error message
-        // process::exit(1)
+    if !valid_expression_str(&command_line_arg, &regex_pat) {
+        println!("{}", "Invalid Input!");
+        process::exit(0);
+    }
 
     // check explicit division by 0
     if clean_white_space_str.contains("/0") {
@@ -65,6 +74,9 @@ fn main_one_time_eval(command_line_arg: String, varib_map: &mut HashMap<String, 
 
 /// main user input while loop
 fn main_user_input_loop(varib_map: &mut HashMap<String, String>) {
+
+    let regex_pat = Regex::new(r"^[a-z 0-9+\-*/()=<>]*$").unwrap();
+
     let mut user_input = "".to_string();
 
     while user_input.trim() != "quit" {
@@ -76,9 +88,11 @@ fn main_user_input_loop(varib_map: &mut HashMap<String, String>) {
         // clean all white space from string and any trailing or heading escape chars
         let clean_white_space_str = remove_all_white_space_from_string(user_input.trim());
 
-        // ***CHECK FOR VALID INPUT HERE!!****
-            // if not valid - print error message
-            // continue;
+        // check for valid input - if not go to next iteration and wait for user input
+        if !valid_expression_str(&clean_white_space_str, &regex_pat) {
+            println!("{}", "Invalid Input!");
+            continue;
+        }
 
         // check explicit division by 0
         if clean_white_space_str.contains("/0") {
@@ -624,3 +638,261 @@ fn test_get_variable_value() {
     assert_eq!("10".to_string(), get_variable_value("variable1".to_string(),
                                                     &mut sample_hash_map));
 }
+
+// ---------------- Valid Expression Check Functions ------------------------------------
+
+/// makes sure there are only allowed characters in string
+fn check_only_valid_chars(expr_str: &str, reg: &Regex) -> bool {
+    // check for empty string
+    if expr_str == "" || expr_str == "(" {
+        return false
+    }
+    // return T/F based on matching regex
+    // ^[a-z 0-9\+\-\*\/\(\)\=\<\>]*$
+    return reg.is_match(expr_str);
+}
+
+#[test]
+fn test_check_only_valid_chars() {
+    let regex_pat: &Regex = &Regex::new(r"^[a-z 0-9+\-*/()=<>]*$").unwrap();
+    assert_eq!(true, check_only_valid_chars("1234567890-+><=()qwertyuioplkjhgfdsazxcvbnm",
+                                            regex_pat));
+    assert_eq!(false, check_only_valid_chars("1234%567890-+><=()qwer@tyuioplkjhgfdsazxcvbnm",
+                                            regex_pat));
+    assert_eq!(false, check_only_valid_chars("1234567890-+><=()qwerMtyuioplkjhgfdsazxcvbnm",
+                                            regex_pat));
+    assert_eq!(false, check_only_valid_chars("1234567890-+><=()qw&ertyuioplkjhgfdsazxcvbnm",
+                                            regex_pat));
+    assert_eq!(false, check_only_valid_chars("1234567890-+><=()QWERTYUIOPLKJHGFDSAZXCVBNM",
+                                             regex_pat));
+    assert_eq!(false, check_only_valid_chars("", regex_pat))
+}
+
+/// assumes check valid char has already accounted for empty string
+fn check_first_char_in_exp(expr_str: &str) -> bool {
+    let first_ch = expr_str.chars().nth(0).unwrap();
+    if (first_ch.is_ascii_lowercase() || first_ch.is_ascii_digit() || first_ch == '(') &&
+        first_ch != '0' {
+        return true
+    }
+    false
+}
+
+#[test]
+fn test_check_first_char_in_exp() {
+    assert_eq!(false, check_first_char_in_exp("098usidk+o12"));
+    assert_eq!(false, check_first_char_in_exp("+xf34trwcf4"));
+    assert_eq!(false, check_first_char_in_exp("-sdfse"));
+    assert_eq!(true, check_first_char_in_exp("asads"));
+    assert_eq!(false, check_first_char_in_exp("<sdwe4"));
+    assert_eq!(true, check_first_char_in_exp("423rw3rf"));
+    assert_eq!(true, check_first_char_in_exp("(dw34rw"));
+    assert_eq!(false, check_first_char_in_exp(")cwe4rcw"));
+    assert_eq!(false, check_first_char_in_exp("=dqwrexw"));
+    assert_eq!(false, check_first_char_in_exp("0"));
+    assert_eq!(true, check_last_char(")"));
+}
+
+fn check_last_char(expr_str: &str) -> bool {
+    let last_char = expr_str.chars().last().unwrap();
+    if expr_str.len() == 1 || last_char.is_ascii_alphanumeric() || last_char == ')' {
+        return true
+    }
+    return false
+}
+
+#[test]
+fn test_check_last_char() {
+    assert_eq!(true, check_last_char("34rdqq34"));
+    assert_eq!(true, check_last_char("34rdqq3s"));
+    assert_eq!(true, check_last_char("(34rdqq3)"));
+    assert_eq!(false, check_last_char("34rdqq34("));
+    assert_eq!(false, check_last_char("34rdqq34-"));
+    assert_eq!(false, check_last_char("34rdqq34*"));
+    assert_eq!(false, check_last_char("34rdqq34>"));
+    assert_eq!(false, check_last_char("34rdqq34="));
+    assert_eq!(false, check_last_char("34rdqq34/"));
+    assert_eq!(true, check_last_char("3"));
+}
+
+/// checks if there are matching pairs of parentheses
+fn count_valid_parentheses(expr_str: &str) -> bool {
+    let mut num_open_parentheses = 0;
+    let mut num_close_parentheses = 0;
+    for ch in expr_str.chars() {
+        if ch == '(' {num_open_parentheses = num_open_parentheses + 1;}
+        else if ch == ')' {num_close_parentheses = num_close_parentheses + 1;}
+    }
+    return num_open_parentheses == num_close_parentheses
+}
+
+#[test]
+fn test_count_valid_parentheses() {
+    assert_eq!(true, count_valid_parentheses("((3+3)/2*3)/3"));
+    assert_eq!(false, count_valid_parentheses("((3+3)/2*3/3"));
+    assert_eq!(false, count_valid_parentheses("(3+3/2*3/3"));
+    assert_eq!(false, count_valid_parentheses("3+3/2*3)/3"));
+    assert_eq!(false, count_valid_parentheses("3+3)/2*3)/3"));
+    assert_eq!(true, count_valid_parentheses("3+3/2*3/3"));
+    assert_eq!(true, count_valid_parentheses("(((er43r((fs((3+3)/2*3)/3)))sfe))66yr"));
+}
+
+/// assumes valid first character and valid last char - checks for any syntax errors in expression string
+fn check_expr_syntax(expr_str: &str) -> bool {
+    let char_vec = expression_string_to_char_vector(expr_str);
+    let mut char_vec_iter = char_vec.iter().peekable();
+    let mut next_char: char;
+    while let Some(current_char) = char_vec_iter.next() {
+        if char_vec_iter.peek() != None {
+            next_char = **char_vec_iter.peek().unwrap();
+        }
+        else {
+            break;
+        }
+
+        if current_char.is_ascii_digit() {
+            match next_char.is_ascii_alphabetic() {
+                true => return false,
+                _ => (),
+            }
+            if next_char == '(' {return false};
+        }
+        else if current_char.is_ascii_alphabetic() {
+            match next_char {
+                '(' => return false,
+                _ => (),
+            }
+        }
+        else if *current_char == ')' {
+            if next_char.is_ascii_alphanumeric() || next_char == '(' {
+                return false
+            }
+        }
+
+        else if *current_char == '+' || *current_char == '-' || *current_char == '*' ||
+            *current_char == '/' || *current_char == '(' {
+            match next_char {
+                ')'|'+'|'-'|'*'|'/'|'<'|'>'|'=' => return false,
+                _ => (),
+            }
+        }
+        else {
+            match next_char {
+                ')'|'+'|'-'|'*'|'/'|'<'|'>' => return false,
+                _ => (),
+            }
+        }
+    }
+    return true;
+}
+
+#[test]
+fn test_check_expr_syntax() {
+    assert_eq!(true, check_expr_syntax("(((32+6)+2*5)-8)/2"));
+    assert_eq!(false, check_expr_syntax("(((32+6)+2*5)-8)/2(3+2)"));
+    assert_eq!(false, check_expr_syntax("(((32+6)+2*5)-8)/joe(3+2)"));
+    assert_eq!(false, check_expr_syntax("(((+32+6)+2*5)-8)/2"));
+    assert_eq!(false, check_expr_syntax("(((32+6)>+2*5)-8)/2"));
+    assert_eq!(false, check_expr_syntax("(((32+6)++2*5)-8)/2"));
+    assert_eq!(false, check_expr_syntax("(((32+6)+2*5)-8)/2<<8"));
+    assert_eq!(false, check_expr_syntax("(((32+6)+2*5)-8)/2=>9"));
+    assert_eq!(false, check_expr_syntax("(((32+6)+2*5)-8)/2=+"));
+    assert_eq!(true, check_expr_syntax("(((32+6)+2*5)-8)/2=="));
+    assert_eq!(true, check_expr_syntax("9"));
+    assert_eq!(true, check_expr_syntax("a"))
+}
+
+/// test ALL string coming in
+fn valid_expression_str(expr_str: &str, regx: &Regex) -> bool {
+    if !check_only_valid_chars(expr_str, regx) {return false};
+    if !check_first_char_in_exp(expr_str) {return false};
+    if !check_last_char(expr_str) {return false};
+    if !count_valid_parentheses(expr_str) {return false};
+    if !check_expr_syntax(expr_str) {return false};
+    return true
+}
+
+#[test]
+fn test_valid_expression_str() {
+    let regex_pat: &Regex = &Regex::new(r"^[a-z 0-9+\-*/()=<>]*$").unwrap();
+    assert_eq!(true, valid_expression_str("(((32+6)+2*5)-8)/2", regex_pat));
+    assert_eq!(false, valid_expression_str("adrw34w4", regex_pat));
+    assert_eq!(false, valid_expression_str("(", regex_pat));
+    assert_eq!(false, valid_expression_str("((+2*5)-8)/2", regex_pat));
+    assert_eq!(false, valid_expression_str("((()+2*5)-8)/2", regex_pat));
+    assert_eq!(false, valid_expression_str("", regex_pat));
+    assert_eq!(false, valid_expression_str("+(((32+6)+2*5)-8)/2", regex_pat));
+    assert_eq!(false, valid_expression_str("(((32+6)+2*5)-8)/2l", regex_pat));
+    assert_eq!(true, valid_expression_str("kkkkkkkkkkkk", regex_pat));
+    assert_eq!(true, valid_expression_str("joe+3", regex_pat));
+    assert_eq!(true, valid_expression_str("(joe+3)", regex_pat));
+    assert_eq!(true, valid_expression_str("3*joe+90+e4", regex_pat));
+    assert_eq!(true, valid_expression_str("3", regex_pat));
+}
+
+fn print_help_text() {
+    let help_text = "\nAuthor: Joe Matta
+Copyright 2018 - Joe Matta. All rights reserved.
+
+This program aims to imitate bc which is a command line calculator. Typing bc at the command line of most linux systems
+will bring up a copyright notice then the program waits for you to type in a bit of math, it will then evaluate that math
+and print an answer, then wait for the user to type more.
+
+This implementation of bc does the following:
+
+supports arithmetic (+ - * /)
+supports parenthesis including nested parentheses
+supports variables -
+    variables that are referenced but not yet assigned automatically get a value of zero.
+    allows assignment to variables using =
+supports comparisons < =< etc. (return 1 for true and 0 for false)
+supports passing arithmetic expressions on the command line as command line parameters when starting matta_bc
+eg:
+entering the following in the command line: matta_bc 5+3
+    will invoke matta_bc for the single computation, print the answer of 8 and then exit the program.
+help (this document) is available through the help flag: matta_bc --help
+    - this will bring up this document explaining how to use matta_bc
+
+To run the program from the command line:
+Navigate to the folder that holds the executable matta_bc (file with no extension) and type the following:
+$ ./matta_bc
+Calling the program with no command line arguments will allow the user to enter an expression and obtain a result as
+many times as they would like until 'quit' is entered. Entering 'quit' ends the program.
+The user may pass a command line argument:
+$ ./matta_bc 5+3
+If more that one argument is passed to the program call, an error will occur.
+$ ./matta_bc 5+3 7-2  ---> error
+If the user includes spaces, parentheses, '<', or '>', single quotes are required around the argument. Some other cases
+may exist that necessitate the use of quotes. Thus, the user is advised to always use quotes when passing a command line
+argument.
+$ ./matta_bc '(3+3)/2'
+
+notes:
+- Multiple digit numbers cannot have spaces between digits (ex. 32 cannot be expressed as 3 2) when passing an expression
+    as a command line argument without quotes
+- Multiplication cannot be done by (2)(3) or 2(3), must be (2)*(3) or 2*(3).
+- Variable names must start with a lowercase letter. They may only contain lowercase letters and numbers.
+- Variable names must end in numbers, if they include numbers. Varibale names cannot have numbers inside name
+    (ie. 'var3iable' is not a valid variable name, but 'variable33' is)
+- When passing a command line argument without quotes around it, argument cannot contain spaces or parentheses
+- Variables can NOT be assigned to other variables (ie. 'variable0 = variable1' is not valid)
+- If assigning a value to a variable, user input must start with a lowercase letter followed by any number of any
+    combination of lowercase letters and then followed by numbers and then '='. The assigned value may be a
+    mathematical expression or a comparison of expressions.
+- Dividing by 0 explicitly (ie. entering '3/0') will cause the an error and the user will be prompted to enter a valid
+    expression. Dividing by 0 implicitly (ie. entering '3/(2-2)' will cause a fatal error; a message will be printed
+    and the program will exit.
+- When entering an expression as a command line argument, the user is required to surround the expression with
+    single quotes in some cases, specifically in the case of comparisons using '<' or '>'. Single quotes must also be
+    used when the expression contains parentheses. For most other cases, the expression can be entered without being
+    surrounded by quotes. This is the result of this notation coinciding with bash commands.
+    Example: 34<=100 must be entered as '34<=100' as a command line argument
+    Example: (3+3)/2 must be entered as '(3+3)/2' as a command line argument
+
+source for expression evaluation algorithm:
+https://www.geeksforgeeks.org/expression-evaluation/\n";
+    println!("{}", help_text)
+}
+
+
+
